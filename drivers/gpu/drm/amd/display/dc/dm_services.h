74d219ecbe678dc1ee873aa9fa69ccf35e506d02
Krzysztof Nowicki <krissn@op.pl>
2017-01-26 08:58:40 +0100

    drm/amd/display: Fix compile error with non-debug kernel

--- drivers/gpu/drm/amd/display/dc/dm_services.h
+++ drivers/gpu/drm/amd/display/dc/dm_services.h
@@ -68,6 +68,12 @@
 
 #define BREAK_TO_DEBUGGER() ASSERT(0)
 
+#else /* CONFIG_DEBUG_KERNEL || CONFIG_DEBUG_DRIVER */
+
+#define ASSERT(expr)
+#define ASSERT_CRITICAL(expr)
+#define BREAK_TO_DEBUGGER()
+
 #endif /* CONFIG_DEBUG_KERNEL || CONFIG_DEBUG_DRIVER */
 
 #define DC_ERR(...)  do { \
